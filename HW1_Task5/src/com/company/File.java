f40c25dbfbbc36434328706dcccdb1dd348e149a
Konstantin Zakhozhev <100844988+Qotyka@users.noreply.github.com>
2022-03-25 20:51:32 +0700

    Create File.java

---
+++ HW1_Task5/src/com/company/File.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class File extends AbstractFileSystemNode{
+    String extention = "";
+    //переопределям метод возвращения имени файла
+    public String getElementName(){
+        return name+"/"+extention;
+    }
+    public String getExtention(){
+        return extention;
+    }
+}
