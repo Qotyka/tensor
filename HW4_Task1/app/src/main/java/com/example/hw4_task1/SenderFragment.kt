0863c57c9c8dc2141d1b6b4ab62229e9baa52307
Konstantin Zakhozhev <k.zakhozhev@g.nsu.ru>
2022-04-12 23:05:51 +0700

    add

---
+++ HW4_Task1/app/src/main/java/com/example/hw4_task1/SenderFragment.kt
@@ -0,0 +1,35 @@
+package com.example.hw4_task1
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SenderFragment: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
